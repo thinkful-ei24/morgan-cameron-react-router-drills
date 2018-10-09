e8d21a08934f8cacdbad4f07727d4911acfd1728
Glitch (react-email-starting-point) <none>
2018-10-09 13:22:09 -0500

    added redirect to inbox at root page

--- src/components/email.js
+++ src/components/email.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
 import Sidebar from './sidebar';
 import SingleEmail from './single-email';
 import EmailList from './email-list';
@@ -11,6 +11,7 @@ export default function Email() {
             <div className="email">
                 <Sidebar />
                 <main>
+                    <Route exact path='/' render={() => <Redirect to='/inbox' />} />
                     <Route exact path='/:folderId' component={EmailList} />
                     < Route exact path='/:folderId/:emailId' component={SingleEmail} />
                 </main>
